07bc5895616e3c4253fdb221f5e0bb8b10ef60ed
Fatih Salim <ahmetfatih.salim@gurmen.com.tr>
2023-08-25 21:55:22 +0300

    Adding ability to list categories. The line in the category controller must be moved to a service instead

--- SaleStore/Controllers/CategoryController.cs
+++ SaleStore/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SaleStore.Data;
+using SaleStore.Models;
 
 namespace SaleStore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List <Category> categories = _db.Categories.ToList(); // This should be in a service. This looks disgusting
+            return View(categories);
         }
     }
 }
