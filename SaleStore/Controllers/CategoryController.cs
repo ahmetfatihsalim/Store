58604765d281566d694310ffe03e9088ea9fcb95
Fatih Salim <ahmetfatih.salim@gurmen.com.tr>
2023-09-01 20:24:02 +0300

    adding delete functionality

--- SaleStore/Controllers/CategoryController.cs
+++ SaleStore/Controllers/CategoryController.cs
@@ -72,5 +72,37 @@ namespace SaleStore.Controllers
             }
             return View();
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")] // can't name get and post actions same so we add action name property
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
