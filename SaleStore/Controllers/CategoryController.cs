e24a73b3d18fa974f64ace04b6664c84280be77f
Fatih Salim <ahmetfatih.salim@gurmen.com.tr>
2023-09-01 20:08:48 +0300

    adding edit functionality and delete button

--- SaleStore/Controllers/CategoryController.cs
+++ SaleStore/Controllers/CategoryController.cs
@@ -43,5 +43,34 @@ namespace SaleStore.Controllers
             //return RedirectToAction("Index"/*"Category"*/); // if it was in different Controller, you also pass the controller name as well
             return View();
         }
+
+        public IActionResult Edit(int? id) 
+        {
+            if (id==null || id==0)
+            {
+                return NotFound(); // We can also use any error view
+            }
+            //Different ways to retrive data from database
+            Category? category = _db.Categories.Find(id); // fastest in this case
+            //Category? category1 = _db.Categories.FirstOrDefault(category => category.ID == id);
+            //Category? category2 = _db.Categories.Where(category => category.ID == id).FirstOrDefault();
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category) 
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+            }
+            return View();
+        }
     }
 }
