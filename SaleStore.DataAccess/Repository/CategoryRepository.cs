d400b4d09a5f43d8f6403f187f31ddaeb293e65e
Fatih Salim <ahmetfatih.salim@gurmen.com.tr>
2023-09-10 19:08:18 +0300

    Creating ICategoryRepository interface, CategoryRepository class and implementing repository pattern.

---
+++ SaleStore.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using SaleStore.Data;
+using SaleStore.DataAccess.Repository.IRepository;
+using SaleStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleStore.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository // We also inherit Repository class. So we dont redefine our crud functions
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db) // whatever db context implementation we gave, passes to base class (which is Repository.cs)
+        {
+            _db= db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _db.Categories.Update(category);
+        }
+    }
+}
---
+++ SaleStore.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using SaleStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleStore.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category> // now we give IRepository it's class
+    {
+        void Update(Category category);
+        void Save();
+    }
+}
