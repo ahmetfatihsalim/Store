c11cae658907788018b696f6212e76fed8f25814
Fatih Salim <ahmetfatih.salim@gurmen.com.tr>
2023-09-22 22:29:00 +0300

    - Adding Upsert functionality rather than seperate Create and Update functionalities - Only Upsert HttpGet works. Upsert HttpPost is broken. I commited this seperately to review it easier in the future (I mean if I ever :) ) - Deleting Controller Folder for it's not needed anymore

--- SaleStore/Areas/Admin/Controllers/ProductController.cs
+++ SaleStore/Areas/Admin/Controllers/ProductController.cs
@@ -22,115 +22,161 @@ namespace SaleStore.Areas.Admin.Controllers
             return View(categories);
         }
 
-        public IActionResult Create() // View name and action name must match
+        #region Create Update Actions Before Upsert
+        //public IActionResult Create() // View name and action name must match
+        //{
+        //    IEnumerable<SelectListItem> CategoryList = _unitOfWork.CategoryRepository.GetAll().Select(/*Projection*/dropdownItem => new SelectListItem
+        //    {
+        //        Text = dropdownItem.Name,
+        //        Value = dropdownItem.ID.ToString()
+        //    }); // for dropdowns
+
+        //    #region Alternative structures to pass data from controller to view
+
+        //    #region ViewBag
+        //    /*
+        //     * Passes data from controller to view but now vice versa
+        //     * Ideal for situations which the temporary data is not in a model
+        //     * Dynamically typecasted
+        //     * It's a dynamic property added in C# 4.0
+        //     * Any number of properties and values can be assigned to viewbag
+        //     * It's life time only lasts during the current http request
+        //     * It's values will be null in case of any redirection
+        //     * Actually just a wrapper around viewdata
+        //     */
+
+        //    //ViewBag.CategoryList/*name can be anything*/ = CategoryList;
+        //    #endregion
+        //    #region ViewData
+        //    /*
+        //     * Passes data from controller to view but now vice versa
+        //     * Ideal for situations which the temporary data is not in a model
+        //     * Must be typecasted before use
+        //     * It's life time only lasts during the current http request
+        //     * It's values will be null in case of any redirection
+        //     */
+
+        //    //ViewData["CatergoryList"] = CategoryList;
+        //    #endregion
+        //    // Internally ViewBag inserts data to ViewData dictionary so key of ViewData and property of ViewBag must not match
+        //    #region TempData
+        //    /* Can be used to store data between two consecutive requests
+        //     * Internally uses session to store the data. More like a short lived session
+        //     * Must be typecasted before use
+        //     * Can be used to store one time messages or error messages
+        //     */
+        //    #endregion
+
+        //    #endregion
+
+        //    #region ViewModel
+        //    /* ViewModel is strongly typed to view
+        //     * This type of views are called strongly typed views. There is a model which is specific for the view
+        //     */
+        //    ProductViewModel productViewModel = new ProductViewModel()
+        //    {
+        //        CategoryList = CategoryList,
+        //        Product = new Product()
+        //    };
+        //    #endregion
+
+        //    return View(productViewModel); // If you define the object in the view there is no need to give any object as parameter. Except if you repopulate the view which in this case we do
+        //}
+
+        //[HttpPost]
+        //public IActionResult Create(ProductViewModel productViewModel) // REST or something
+        //{
+        //    if (ModelState.IsValid) // checking validations
+        //    {
+        //        _unitOfWork.ProductRepository.Add(productViewModel.Product);
+        //        _unitOfWork.Save();
+        //        TempData["Success"] = "Product created successfully"; // To show error messages
+        //    }
+        //    else // if dropdown value is not valid. Not needed in our case. We use ValidateNever. Also we need to repopulate the dropdown for another request after an error, so...
+        //    {
+        //        productViewModel.CategoryList = _unitOfWork.CategoryRepository.GetAll().Select(dropdownItem => new SelectListItem()
+        //        {
+        //            Text= dropdownItem.Name,
+        //            Value = dropdownItem.ID.ToString()
+        //        });
+        //    }
+        //    //return RedirectToAction("Index"/*"Product"*/); // if it was in different Controller, you also pass the controller name as well
+        //    return View(productViewModel);
+        //}
+
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound(); // We can also use any error view
+        //    }
+        //    //Different ways to retrive data from database
+        //    Product? product = _unitOfWork.ProductRepository.GetFirstOrDefault(u => u.ID == id); // fastest in this case
+        //    //Product? product1 = _db.Categories.FirstOrDefault(product => product.ID == id);
+        //    //Product? product2 = _db.Categories.Where(product => product.ID == id).FirstOrDefault();
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(product);
+        //}
+
+        //[HttpPost]
+        //public IActionResult Edit(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.ProductRepository.Update(product);
+        //        _unitOfWork.Save();
+        //        TempData["Success"] = "Product edited successfully";
+        //    }
+        //    return View();
+        //}
+        #endregion
+
+        public IActionResult Upsert(int? id) // Update and insert in single page/action
         {
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.CategoryRepository.GetAll().Select(/*Projection*/dropdownItem => new SelectListItem
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.CategoryRepository.GetAll().Select(dropdownItem => new SelectListItem
             {
                 Text = dropdownItem.Name,
                 Value = dropdownItem.ID.ToString()
-            }); // for dropdowns
+            });
 
-            #region Alternative structures to pass data from controller to view
-
-            #region ViewBag
-            /*
-             * Passes data from controller to view but now vice versa
-             * Ideal for situations which the temporary data is not in a model
-             * Dynamically typecasted
-             * It's a dynamic property added in C# 4.0
-             * Any number of properties and values can be assigned to viewbag
-             * It's life time only lasts during the current http request
-             * It's values will be null in case of any redirection
-             * Actually just a wrapper around viewdata
-             */
-
-            //ViewBag.CategoryList/*name can be anything*/ = CategoryList;
-            #endregion
-            #region ViewData
-            /*
-             * Passes data from controller to view but now vice versa
-             * Ideal for situations which the temporary data is not in a model
-             * Must be typecasted before use
-             * It's life time only lasts during the current http request
-             * It's values will be null in case of any redirection
-             */
-
-            //ViewData["CatergoryList"] = CategoryList;
-            #endregion
-            // Internally ViewBag inserts data to ViewData dictionary so key of ViewData and property of ViewBag must not match
-            #region TempData
-            /* Can be used to store data between two consecutive requests
-             * Internally uses session to store the data. More like a short lived session
-             * Must be typecasted before use
-             * Can be used to store one time messages or error messages
-             */
-            #endregion
-
-            #endregion
-
-            #region ViewModel
-            /* ViewModel is strongly typed to view
-             * This type of views are called strongly typed views. There is a model which is specific for the view
-             */
             ProductViewModel productViewModel = new ProductViewModel()
             {
                 CategoryList = CategoryList,
                 Product = new Product()
             };
-            #endregion
-
-            return View(productViewModel); // If you define the object in the view there is no need to give any object as parameter. Except if you repopulate the view which in this case we do
+            if (id == null || id == 0) // create
+            {
+                return View(productViewModel);
+            }
+            else // update
+            {
+                productViewModel.Product = _unitOfWork.ProductRepository.GetFirstOrDefault(product => product.ID == id);
+                return View(productViewModel);
+            }
         }
 
-        [HttpPost]
-        public IActionResult Create(ProductViewModel productViewModel) // REST or something
-        {
-            if (ModelState.IsValid) // checking validations
-            {
-                _unitOfWork.ProductRepository.Add(productViewModel.Product);
-                _unitOfWork.Save();
-                TempData["Success"] = "Product created successfully"; // To show error messages
-            }
-            else // if dropdown value is not valid. Not needed in our case. We use ValidateNever. Also we need to repopulate the dropdown for another request after an error, so...
-            {
-                productViewModel.CategoryList = _unitOfWork.CategoryRepository.GetAll().Select(dropdownItem => new SelectListItem()
-                {
-                    Text= dropdownItem.Name,
-                    Value = dropdownItem.ID.ToString()
-                });
-            }
-            //return RedirectToAction("Index"/*"Product"*/); // if it was in different Controller, you also pass the controller name as well
-            return View(productViewModel);
-        }
-
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound(); // We can also use any error view
-            }
-            //Different ways to retrive data from database
-            Product? product = _unitOfWork.ProductRepository.GetFirstOrDefault(u => u.ID == id); // fastest in this case
-            //Product? product1 = _db.Categories.FirstOrDefault(product => product.ID == id);
-            //Product? product2 = _db.Categories.Where(product => product.ID == id).FirstOrDefault();
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            return View(product);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product product)
+        [HttpPost] // TO-DO
+        public IActionResult Upsert(ProductViewModel productViewModel, IFormFile? file /*for ImageUrl*/) 
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.ProductRepository.Update(product);
+                _unitOfWork.ProductRepository.Add(productViewModel.Product);
                 _unitOfWork.Save();
-                TempData["Success"] = "Product edited successfully";
+                TempData["Success"] = "Product created successfully";
             }
-            return View();
+            else
+            {
+                productViewModel.CategoryList = _unitOfWork.CategoryRepository.GetAll().Select(dropdownItem => new SelectListItem()
+                {
+                    Text = dropdownItem.Name,
+                    Value = dropdownItem.ID.ToString()
+                });
+            }
+            return View(productViewModel);
         }
 
         public IActionResult Delete(int? id)
